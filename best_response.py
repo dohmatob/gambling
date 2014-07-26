a053f23546a5ddb57cc530edcb76f9faea0c2aca
DOHMATOB Elvis Dopgima <elvis.dohmatob@inria.fr>
2014-07-26 17:31:50 +0200

    ENH: better convergence criterion

--- best_response.py
+++ best_response.py
@@ -32,26 +32,31 @@ def best_response(A, E, e, y0, max_iter=1000, tol=1e-2):
     tau = sigma = .99 / L
     assert tau * sigma * L * L < 1.
     const = tau * A.T.dot(y0)
-    r = 0
-    history = []
+    average_error = 0
+    average_errors = []
     for k in xrange(max_iter):
         old_x = x.copy()
-        residue = e - E.dot(xbar)
-        r = (k * r + np.dot(residue, residue)) / (k + 1.)
-        history.append(r)
-        print "Iteration %03i/%03i: running average ||e - Exk||^2 = %g" % (
-            k + 1, max_iter, r)
-        if r < tol:
-            print "Converged (running average ||e - Exk||^2 < %g)" % tol
-            break
-        zeta += sigma * (residue)
+        old_zeta = zeta.copy()
+        zeta += sigma * (e - E.dot(xbar))
         x += const + E.T.dot(zeta)
 
         x = np.maximum(x, 0.)
-
         xbar = 2 * x - old_x
 
-    return x, history
+        a = x - old_x
+        b = zeta - old_zeta
+        average_error = (k * average_error + .5 * (np.dot(a, a) / tau + np.dot(
+                    b, b) / sigma))  / (k + 1.)
+        average_errors.append(average_error)
+        print ("Iteration %04i/%04i: .5 * (||x^(k+1) - "
+               "x^(k)||^2" " / tau + ||zeta^(k+1) - zeta^(k)||^2 / sigma) "
+               "= %.3e" % (k + 1, max_iter, average_error))
+        if average_error < tol:
+            print ("Converged (.5 * (||x^(k+1) - x^(k)||^2 / tau + "
+                   "||zeta^(k+1) - zeta^(k)||^2 / sigma) < %g)" % tol)
+            break
+
+    return x, average_errors
 
 
 if __name__ == "__main__":
@@ -62,14 +67,15 @@ if __name__ == "__main__":
     E = np.array([[1, 0, 0, 0, 0], [-1, 1, 1, 0, 0], [-1, 0, 0, 1, 1]])
     e = np.eye(E.shape[0])[0]
     y0 = np.array([1, .66, .33])
-    xstar, history = best_response(A, E, e, y0)
+    xstar, average_errors = best_response(A, E, e, y0)
     print "x* =", xstar
     print E.dot(xstar) - e
 
     import pylab as pl
-    history = np.array(history)
-    history -= history.min()
-    pl.loglog(history)
-    pl.ylabel("(excess) running average ||e - Exk||^2")
+    average_errors = np.array(average_errors)
+    average_errors -= average_errors.min()
+    pl.loglog(average_errors)
+    pl.ylabel("(excess) running average .5 * (||x^(k+1) - x^(k)||^2 / tau + "
+              "||zeta^(k+1) - zeta^(k)||^2 / sigma)")
     pl.xlabel("k")
     pl.show()
