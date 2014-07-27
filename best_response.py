64ef5d55aa5ab9fa235be69ad28c0724f8579f80
DOHMATOB Elvis Dopgima <elvis.dohmatob@inria.fr>
2014-07-27 13:27:00 +0200

    looks much better

--- best_response.py
+++ best_response.py
@@ -1,8 +1,10 @@
+from math import sqrt
 import numpy as np
 from scipy import linalg
 
 
-def best_response(A, E, e, y0, max_iter=1000, tol=1e-2, callback=None):
+def best_response(A, E, e, y0, max_iter=1000, tol=1e-2, theta=1.,
+                  callback=None):
     """Computes best response to an opponent's realization plan in a two-person
     sequential game.
 
@@ -35,9 +37,6 @@ def best_response(A, E, e, y0, max_iter=1000, tol=1e-2, callback=None):
     average_error = 0
     average_errors = []
     for k in xrange(max_iter):
-        if callback:
-            callback(locals())
-
         old_x = x.copy()
         old_zeta = zeta.copy()
 
@@ -47,23 +46,26 @@ def best_response(A, E, e, y0, max_iter=1000, tol=1e-2, callback=None):
 
         # primal update
         x = np.maximum(x, 0.)
-        xbar = 2 * x - old_x
+        xbar = x + theta * (x - old_x)
+
+        if callback:
+            callback(locals())
 
         # check primal-dual convergence
         a = x - old_x
         b = zeta - old_zeta
-        average_error = .5 * (np.dot(a, a) / tau + np.dot(
+        error = .5 * (np.dot(a, a) / tau + np.dot(
                     b, b) / sigma)
-        average_errors.append(average_error)
-        print ("Iteration %04i/%04i: running average .5 * (||x^(k+1) - "
+        errors.append(error)
+        print ("Iteration %04i/%04i: .5 * (||x^(k+1) - "
                "x^(k)||^2" " / tau + ||zeta^(k+1) - zeta^(k)||^2 / sigma) "
-               "= %.3e" % (k + 1, max_iter, average_error))
-        if average_error < tol:
-            print ("Converged (running average .5 * (||x^(k+1) - x^(k)||^2"
-                   " / tau + ||zeta^(k+1) - zeta^(k)||^2 / sigma) < %g)" % tol)
+               "= %.3e" % (k + 1, max_iter, error))
+        if error < tol:
+            print ("Converged (.5 * (||x^(k+1) - x^(k)||^2 / tau + "
+                   "||zeta^(k+1) - zeta^(k)||^2 / sigma) < %.3e)" % tol)
             break
 
-    return x, average_errors
+    return x, zeta, errors
 
 
 if __name__ == "__main__":
@@ -73,29 +75,67 @@ if __name__ == "__main__":
     A = A.T
     E = np.array([[1, 0, 0, 0, 0], [-1, 1, 1, 0, 0], [-1, 0, 0, 1, 1]])
     e = np.eye(E.shape[0])[0]
-    y0 = np.array([1, .66, .33])
+    y0 = np.array([1., 2. / 3., 1. / 3.])
 
     import pylab as pl
+    from matplotlib import lines
 
-    def cb(env):
-        pl.scatter(env['x'][0], env['zeta'][0], marker="*")
+    xstar = [1., 0., 1., 0., 1.]
+    zetastar = [0., 0., 0.]
+
+    pl.figure(figsize=(13, 7))
+    thetas = [0., .5, 1.]
+    for i, theta in enumerate(thetas):
+        mind = []
+
+        def cb(env):
+            v = env['x'] - xstar
+            mind.append(sqrt(np.dot(v, v)))
+
+            # draw tangent
+            ax.add_line(lines.Line2D([env['old_x'][0], env['x'][0]],
+                                     [env['old_zeta'][0], env['zeta'][0]]))
+
+            # draw normal
+            ax.add_line(lines.Line2D([xstar[0], env['x'][0]],
+                                     [zetastar[0], env['zeta'][0]]))
+
+            pl.draw()
+
+        ax = pl.subplot("3%i%i" % (len(thetas), i + 1))
+        ax.set_title("theta=%g" % theta)
+        pl.xlim(0., 2.)
+        pl.ylim(-1., 1.)
+        pl.xlabel("xk")
+        if i == 0.:
+            pl.ylabel("yk")
+        pl.ion()
+
+        # draw center
+        pl.scatter(xstar[0], zetastar[0], marker="*")
         pl.draw()
 
-    fig = pl.figure(figsize=(13, 13))
-    pl.grid("on")
-    pl.xlim(0., 2.)
-    pl.ylim(-1., 1.)
-    pl.xlabel("xk")
-    pl.ylabel("yk")
-    pl.ion()
+        xstar, zetastar, errors = best_response(A, E, e, y0, theta=theta,
+                                                callback=cb, tol=1e-8,
+                                                max_iter=200)
+        print "x* =", xstar
+        print "zeta* =", zetastar
+        print "e - Ex* =", E.dot(xstar) - e
 
-    xstar, average_errors = best_response(A, E, e, y0, callback=cb, tol=1e-4)
-    print "x* =", xstar
-    print E.dot(xstar) - e
+        ax = pl.subplot("3%i%i" % (len(thetas), len(thetas) + i + 1))
+        pl.loglog(errors)
+        pl.xlabel("k")
+        # pl.ylabel(".5 * (||x^(k+1) - x^(k)||^2 / tau + "
+        #           "||zeta^(k+1) - zeta^(k)||^2 / sigma)")
+        if i == 0.:
+            pl.ylabel("error")
 
-    pl.figure()
-    pl.semilogx(average_errors, linewidth=3)
-    pl.ylabel("running average .5 * (||x^(k+1) - x^(k)||^2 / tau + "
-              "||zeta^(k+1) - zeta^(k)||^2 / sigma)")
-    pl.xlabel("k")
-    pl.show()
+        ax = pl.subplot("3%i%i" % (len(thetas), 2 * len(thetas) + i + 1))
+        pl.loglog(mind)
+        pl.xlabel("k")
+        # pl.ylabel(".5 * (||x^(k+1) - x^(k)||^2 / tau + "
+        #           "||zeta^(k+1) - zeta^(k)||^2 / sigma)")
+        if i == 0.:
+            pl.ylabel("||x^(k) - x*||")
+
+        pl.show()
