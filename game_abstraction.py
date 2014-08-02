f80b936c3d68f02aa1184f56fbc220fdcf59e27f
DOHMATOB Elvis Dopgima <elvis.dohmatob@inria.fr>
2014-08-02 19:26:37 +0200

    tree abstraction of sequential games

---
+++ game_abstraction.py
@@ -0,0 +1,111 @@
+MOVES = ['fold', 'check', 'call', 'raise']
+
+
+class Game(object):
+    def __init__(self, limit=10):
+        self.limit = limit
+
+    def can_check(self, state, player):
+        """Check that given player can "check" in given state."""
+        return player == state["norminal"] and state["rnd"] < 1.
+
+    def can_call(self, state, _):
+        """Check that given player can "call" in given state."""
+        return state["pot"] + state["bet"] <= self.limit
+
+    def can_raise(self, state, _):
+        """Check that given player can "raise" in given state."""
+        return state["pot"] + state["bet"] + 1 <= self.limit
+
+    def can_fold(self, *unused):
+        """Check that given player can "fold" in given state."""
+        return True
+
+    def can_move(self, state, player, move):
+        """Check where given player can make given move in given state."""
+        assert move in MOVES, move
+        if move == "call":
+            return self.can_call(state, player)
+        elif move == "raise":
+            return self.can_raise(state, player)
+        elif move == "fold":
+            return self.can_fold(state, player)
+        elif move == "check":
+            return self.can_check(state, player)
+        else:
+            RuntimeError("Shouldn't be here!")
+
+    def do_move(self, state, move):
+        if move in ["check", "fold"]:
+            return False
+        elif move in ['raise', 'call']:
+            if move == "raise":
+                state['bet'] += 1
+            state["pot"] += state['bet']
+            assert state["pot"] <= self.limit  # double-check move was possible
+            return move == "raise"
+        else:
+            RuntimeError("Shouldn't be here!")
+
+    def move_id(self, p, move):
+        if move == "kall":
+            move = "call"
+        if p:
+            move = move.upper()
+        return move[0]
+
+    def go(self, state, p, move):
+        stop = not self.do_move(state, move)
+        print state['padding'][:-1] + "+-%s: state=%s" % (
+            self.move_id(p, move), dict((k, state[k])
+                                        for k in ["rnd", "bet", "pot"]))
+        state["padding"] += " "
+        if not stop:
+            q = (p + 1) % 2
+            moves = [m for m in ["fold", "call", "raise"]
+                     if self.can_move(state, q, m)]
+            for cnt, m in enumerate(moves):
+                state_ = state.copy()
+                print state["padding"] + "|"
+                if cnt == len(moves) - 1:
+                    state_["padding"] += " "
+                else:
+                    state_["padding"] += "|"
+                self.go(state_, q, m)
+        else:
+            # entre second round
+            if move != "fold":
+                state['rnd'] += 1
+                if state['rnd'] < 2:
+                    state['norminal'] = 1 - state["norminal"]
+                    # print state['padding'] + "|"
+                    # print state['padding'] + "+-new rnd"
+                    # state["padding"] += " "
+                    return self.play_rnd(state)
+                return False
+
+    def play_rnd(self, state):
+        if state["rnd"] >= 2:
+            return False
+        state["bet"] = 1
+        moves = [m for m in ['check', "fold", "call", "raise"]
+                 if self.can_move(state.copy(), state["norminal"], m)]
+        for cnt, move in enumerate(moves):
+            state_ = state.copy()
+            print state["padding"] + "|"
+            if cnt == len(moves) - 1:
+                state_["padding"] += " "
+            else:
+                state_["padding"] += "|"
+            self.go(state_, state['norminal'], move)
+
+        state['rnd'] += 1
+        return True
+
+    def play_game(self):
+        state = dict(bet=1, pot=0, norminal=0, rnd=0, padding="")
+        print ".: state=%s" % (dict((k, state[k])
+                                    for k in ["bet", "pot", "rnd"]))
+        self.play_rnd(state)
+
+Game(limit=5.).play_game()
--- sequential_games.py
+++ sequential_games.py
@@ -1,8 +1,71 @@
 # Author: DOHMATOB Elvis <gmdopp@gmail.com>
 
 import time
+from math import sqrt
 import numpy as np
 from scipy import linalg
+
+
+def condat_primal_dual(grad_F, prox_G, prox_Hstar, beta, L=None,
+                       norm_L=None, max_iter=100, verbose=0, tol=1e-4,
+                       align=""):
+    """
+    Primal-dual solver for the following sum-of-composite functions
+    minimization problem:
+
+        minimize F(x) + G(x) + H(Lx)
+           x
+    """
+
+    # vars
+    if L is None:
+        norm_L = 1.
+    elif norm_L is None:
+        norm_L = linalg.svdvals(L)[0]
+    if norm_L is None:
+        norm_L = linalg.svdvals(L)[0]
+    sigma = .99 / norm_L
+    tau = .9 / (sigma * norm_L * norm_L + .5 * beta)
+    assert 1. / tau - sigma * norm_L * norm_L >= beta * .5
+    print norm_L, beta, sigma, tau
+    x = np.zeros(L.shape[1])
+    y = np.zeros(L.shape[0])
+    ybar = y.copy()
+    delta = 2. - .5 * beta / (1. / tau - sigma * norm_L * norm_L)
+    assert 1. <= delta <= 2.
+    rho = .99 * delta  # acceleration param
+    assert 0. < rho < delta
+
+    _Ldot = lambda x: x if L is None else L.dot(x)
+    _LTdot = lambda v: v if L is None else L.T.dot(v)
+
+    # main loop
+    for k in xrange(max_iter):
+        old_x = x.copy()
+        old_y = y.copy()
+
+        # primal-dual updates
+        eps = 1.e-2 * 1. / (k + 1.) ** 4.
+        xbar = x - tau * (grad_F(x) + _LTdot(y))
+        xbar = prox_G(xbar, tau, tol=eps)
+        ybar = y + sigma * _Ldot(2. * xbar - x)
+        ybar = prox_Hstar(ybar, sigma, tol=eps)
+        x = rho * xbar + (1. - rho) * old_x
+        y = rho * ybar + (1. - rho) * old_y
+
+        # check convergence
+        error = sqrt((((x - old_x) ** 2).sum() / tau + (
+                    (y - old_y) ** 2).sum() / sigma))
+        if verbose:
+            print ("%sCONDAT teration: %03i/%03i: ||x^(k) - x^(k-1)||^2"
+                   " + ||y^(k) - y^(k-1)||^2 =%g" % (align, k + 1, max_iter,
+                                                     error))
+        if error < tol:
+            if verbose:
+                print "%sConverged after %i iterations." % (align, k + 1)
+            break
+
+    return x
 
 
 def devil(L, r, z, A=None, norm_L=None, norm_A=None, max_iter=100, verbose=0,
@@ -43,10 +106,8 @@ def devil(L, r, z, A=None, norm_L=None, norm_A=None, max_iter=100, verbose=0,
     norm = norm_L
     if A is None:
         norm = max(norm, 1.)
-    else:
-        assert 0
-        if norm_A is None:
-            norm_A = linalg.svdvals(A)[0]
+    elif norm_A is None:
+        norm_A = linalg.svdvals(A)[0]
         norm = max(norm, norm_A)
     sigma = tau = .99 / norm
     assert sigma * tau * norm * norm < 1.
@@ -68,8 +129,7 @@ def devil(L, r, z, A=None, norm_L=None, norm_A=None, max_iter=100, verbose=0,
         old_zeta = zeta.copy()
 
         # Chambolle-Pock formula
-
-        if method == 'chambolle-pock':
+        if method == 'CHAMBOLLE-POCK':
             v += sigma * (_Adot(xbar) - z)
             v /= (1. + sigma)
             zeta += sigma * (L.dot(xbar) - r)
@@ -78,7 +138,7 @@ def devil(L, r, z, A=None, norm_L=None, norm_A=None, max_iter=100, verbose=0,
             xbar = 2 * x - old_x
         else:
             # L. Condat's formula
-            xbar = x - tau * (_Adot(v) + L.T.dot(zeta))
+            xbar = x - tau * (_ATdot(v) + L.T.dot(zeta))
             xbar = np.maximum(xbar, 0.)
             vbar = v + sigma * (_Adot(2 * xbar - x) - z)
             vbar /= (1. + sigma)
@@ -88,9 +148,9 @@ def devil(L, r, z, A=None, norm_L=None, norm_A=None, max_iter=100, verbose=0,
             zeta = rho * zetabar + (1. - rho) * old_zeta
 
         # check convergence
-        error = .5 * (((x - old_x) ** 2).sum() / tau + (
-                ((v - old_v) ** 2).sum() + (
-                    (zeta - old_zeta) ** 2).sum()) / sigma)
+        error = sqrt((((x - old_x) ** 2).sum() / tau + (
+                    ((v - old_v) ** 2).sum() + (
+                        (zeta - old_zeta) ** 2).sum()) / sigma))
         if verbose:
             print "\t%s teration: %03i/%03i: error=%g" % (
                 method, k + 1, max_iter, error)
@@ -102,41 +162,43 @@ def devil(L, r, z, A=None, norm_L=None, norm_A=None, max_iter=100, verbose=0,
     return x
 
 
-def evil(L, r, z, norm=None, max_iter=100, verbose=0, tol=1e-4):
-    """Algorithm from https://www.ljll.math.upmc.fr/~plc/svva1.pdf (p.17).
+def evil(L, r, z, proj_C=lambda v: np.maximum(v, 0.), L_norm=None,
+         max_iter=100, verbose=1, tol=1e-4):
+    """Projects the point z onto the intersection of the polyhedron "Ex=r"
+    and the convex set C.
     """
-    q, n = L.shape
-    if norm is None:
-        norm = linalg.svdvals(L)[0]
-    gamma = 1. / norm ** 2
-    v = np.zeros(q)
-    x = np.zeros(n)
+    if L_norm is None:
+        L_norm = linalg.svdvals(L)[0]
+    gamma = .99 / L_norm ** 2
+    v = np.zeros(L.shape[0])
+    x = np.zeros(L.shape[1])
+    salt = 1e-4
     for k in xrange(max_iter):
         old_x = x.copy()
-        old_v = v.copy()
-        x = np.maximum(z - L.T.dot(v), 0.)
-        v += gamma * (L.dot(x) - r)
-        error = ((x - old_x) ** 2).sum() + ((v - old_v) ** 2).sum()
+        x = proj_C(z - L.T.dot(v)) + salt
+        v += gamma * (L.dot(x) - r + salt)
+        salt *= .5  # we want: \sum_{k}{salt_k} < \infty
+        error = sqrt(((x - old_x) ** 2).sum())
         if verbose:
-            print ("\tIteration: %03i/%03i: x^(k) = %s, v^(k) = %s, "
-                   "||x^(k) - x^(k-1)||^2 + ||v^(k) - v^(k-1)||^2 "
-                   "= %5.3e" % (k + 1, max_iter, x, v, error))
-        if error < tol:
-            print "\tConverged"
+            print "\tIteration %03i/%03i: x=%s, error=%5.2e" % (
+                k + 1, max_iter, x, error)
+        if error < tol and 1:
+            if verbose:
+                print "\tConverged after %i iterations." % (k + 1)
             break
     return x
 
 
 def compute_ne(A, P, Q, a, b, norm_A=None, norm_P=None, norm_Q=None,
                max_iter=1000, callback=None, tol=1e-4, verbose=1,
-               method="condat"):
+               method="chambolle-pock"):
     """Compute Nash Equilibrium point (x*, y*) in a two-person zero-sum
     sequential game in sequential form (in the sense of Bernhard
     von Stengel and co-workers).
 
     The hyperplanes {x: Px=a} and {y: Qy=b} encode the linear constraints
     on the realization plans x and y of players 1 and 2 respectively.
-    A is the payoff matrix.
+    A is the value matrix.
 
     Method is Chambolle-Pock's saddle-point technique.
     """
@@ -163,8 +225,9 @@ def compute_ne(A, P, Q, a, b, norm_A=None, norm_P=None, norm_Q=None,
     eps = 1e-1
 
     # main loop
-    errors = []
+    values = []
     times = []
+    old_value = np.inf
     for k in xrange(max_iter):
         # misc
         old_x = x.copy()
@@ -173,44 +236,58 @@ def compute_ne(A, P, Q, a, b, norm_A=None, norm_P=None, norm_Q=None,
         if method == "CHAMBOLLE-POCK":
             # Chambolle-Pock's primal-dual iteration
             y += sigma * A.dot(xbar)
-            if verbose:
+            if verbose > 1:
                 print "\tDual projection"
-            y = devil(Q, b, y, norm_L=norm_Q, verbose=verbose, tol=eps)
-            x -= tau * A.T.dot(y)
-            if verbose:
-                print "\tPrimal projection"
-            x = devil(P, a, x, norm_L=norm_P, verbose=verbose, tol=eps)
-            eps *= .5
-            xbar = 2 * x - old_x
 
+            y = evil(Q, b, y, verbose=(verbose > 1),
+                     tol=eps, L_norm=norm_Q)
+            # y = devil(Q, b, y, norm_L=norm_Q, tol=eps, method=method,
+            #           verbose=(verbose > 1))
+            x -= tau * A.T.dot(y)
+            if verbose > 1:
+                print "\tPrimal projection"
+            x = evil(P, a, x, verbose=(verbose > 1),
+                     tol=eps, L_norm=norm_P)
+            # x = devil(P, a, x, norm_L=norm_P, verbose=(verbose > 1),
+            #           tol=eps, method=method)
+            eps = max(1e-5, eps * .1)  # decrease eps (but not too much)
+            xbar = 2 * x - old_x
         else:
             # L. Condat's primal-dual iteration
-            eps = 1.e-2 * 1. / (k + 1.) ** 4.
+            # eps = 1.e-2 * 1. / (k + 1.) ** 4.
+            eps = max(1e-5, eps * .5)
             rho = .9  # note that \sum_{k}{\rho_k \epsilon_k} < \infty
-            xbar = devil(P, a, x - tau * A.T.dot(y), verbose=(verbose > 1),
-                         tol=eps, method=method)
-            ybar = devil(Q, b, y + sigma * A.dot(2 * xbar - x),
-                         verbose=(verbose > 1), tol=eps, method=method)
+            xbar = evil(P, a, x - tau * A.T.dot(y), verbose=(verbose > 1),
+                        tol=eps, gamma=1.9 / norm_P ** 2)
+            # xbar = devil(P, a, x - tau * A.T.dot(y), verbose=(verbose > 1),
+            #              tol=eps, method=method)
+            ybar = evil(Q, b, y + sigma * A.dot(2 * xbar - x),
+                        verbose=(verbose > 1), tol=eps,
+                        gamma=1.9 / norm_Q ** 2)
+            # ybar = devil(Q, b, y + sigma * A.dot(2 * xbar - x),
+            #              verbose=(verbose > 1), tol=eps, method=method)
             x = rho * xbar + (1. - rho) * old_x
             y = rho * ybar + (1. - rho) * old_y
 
         # check convergence
-        error = .5 * (((x - old_x) ** 2).sum() / tau + (
-                (y - old_y) ** 2).sum() / sigma)
+        # error = sqrt((((x - old_x) ** 2).sum() / tau + (
+        #             (y - old_y) ** 2).sum() / sigma))
+        value = np.dot(y, A.dot(x))
         times.append(time.time() - tic)
-        errors.append(error)
+        values.append(value)
+        dE = old_value - value
+        old_value = value
         if callback:
             callback(locals())
         if verbose:
-            print ("%s iteration: %03i/%03i: ||x^(k) - x^(k-1)||^2 + ||y^(k)"
-                   " - y^(k-1)||^2 =%g" % (method, k + 1, max_iter,
-                                           error))
-        if error < tol:
+            print "%s iteration: %03i/%03i: value=%5.2e" % (
+                method, k + 1, max_iter, value)
+        if abs(dE) < tol:
             if verbose:
                 print "Converged after %i iterations." % (k + 1)
             break
 
-    return x, y, errors, times
+    return x, y, values, times
 
 if __name__ == "__main__":
     """
@@ -219,6 +296,12 @@ if __name__ == "__main__":
     """
 
     ### Load the game tree ###################################################
+    import scipy.io
+    game = scipy.io.loadmat('game.mat')
+    A, P, Q, a, b = [game[key] for key in "APQab"]
+    A = A.T
+    a = a.ravel()
+    b = b.ravel()
     A = np.zeros((5, 3))
     A[2:4, 1:] = [[1, -1], [-2, 4]]
     A[-1, 0] = 1
@@ -229,33 +312,30 @@ if __name__ == "__main__":
     b = np.eye(Q.shape[0])[0]
 
     ### Visualization callbacks ##############################################
-    i = 2.
+    i = 2
 
     import pylab as pl
-    # from matplotlib import lines
-    # pl.figure(figsize=(15, 7))
-    # ax = pl.subplot("111")
-    # pl.xlabel("xk")
-    # pl.ylabel("yk")
-    # pl.xlim(0., 1.2)
-    # pl.ylim(0., 1.2)
-    # pl.ion()
-    # pl.show()
+    pl.figure(figsize=(15, 7))
+    ax = pl.subplot("111")
+    pl.xlabel("xk")
+    pl.ylabel("yk")
+    pl.xlim(-1, 2)
+    pl.ylim(-1, 2)
+    pl.ion()
+    pl.scatter(0, 0)
+    pl.show()
 
-    # def cb(env):
-    #     # update locus
-    #     ax.add_line(lines.Line2D([env['old_x'][i], env['x'][i]],
-    #                              [env['old_y'][i], env['y'][i]],
-    #                              linestyle="-"))
-
-    #     pl.draw()
+    def cb(env):
+        ax.scatter(env["x"][i], env['y'][i])
+        pl.draw()
 
     ### Run primal-dual algo to compute NE ####################################
-    pl.figure()
-    for method in ["condat", "chambolle-pock"][::-1]:
-        x, y, errors, times = compute_ne(A, P, Q, a, b, tol=1e-6,
-                                         method=method, callback=None,
-                                         verbose=0)
+    times = {}
+    values = {}
+    for method in ["condat", "chambolle-pock"][1:]:
+        x, y, values[method], times[method] = compute_ne(
+            A, P, Q, a, b, method=method, callback=cb,
+            verbose=2, tol=1e-10)
 
         # pl.axvline(x[i], linestyle='--')
         # pl.axhline(y[i], linestyle='--')
@@ -265,6 +345,12 @@ if __name__ == "__main__":
         print "x* = ", x
         print "y* =", y
 
-        pl.loglog(times, errors, label=method)
+    pl.figure()
+    for method in times.keys():
+        pl.plot(times[method], values[method], 's-', label=method)
+    pl.axhline(np.min([v[-1] for v in values.values()]), linestyle="--",
+               label="equilibrium value")
+    pl.ylabel("value")
+    pl.xlabel("time (s)")
     pl.legend(loc="best")
     pl.show()
