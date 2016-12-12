48d84d3135298a75c40632e8e373b857f66cb1c4
Elvis Dohmatob <gmdopp@gmail.com>
2016-12-12 15:41:46 +0100

    more junk files rmved

--- kuhn.py
+++
@@ -1,6 +0,0 @@
-rules = []
-for perm, proba in zip([(1, 2), (1, 3), (2, 1), (2, 3), (3, 1), (3, 2)],
-                       [1. / 6] * 6):
-    head = [((perm, proba), 1)]
-    rules.append(([head, ("pass", 2), ("pass", 3)], (1., None),
-                  [head, ("pass", 2), ("bet", 1), (
